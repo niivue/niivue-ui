0a58151bde728c33a91d3af26f95a29fc14ec893
Taylor Hanayik <hanayik@gmail.com>
2022-06-19 16:45:03 +0100

    fix indentation

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -26,40 +26,40 @@ export function ColorPicker({setColor, prop, title, initialAlpha=1}){
 	}
 
 	return (
-	<Box
-		sx={{
-			display:'flex'
-		}}>
-		<Grid container m={2}>
-			<Grid item marginRight='auto'>
-				<Typography>
-					{title}
-				</Typography>
+		<Box
+			sx={{
+				display:'flex'
+			}}>
+			<Grid container m={2}>
+				<Grid item marginRight='auto'>
+					<Typography>
+						{title}
+					</Typography>
+				</Grid>
+				<Grid item>
+					<Input 
+						disableUnderline={true}
+						type='color'
+						style={{width:'50px', height:'20px'}}
+						onInput={(e)=>{updateColor(e.target.value, colorAlpha);}}
+						value={colorHex}
+					/>
+				</Grid>
+				<Grid item>
+					<Input
+						size='small'
+						style={{marginLeft:4}}
+						value={colorAlpha}
+						inputProps={{
+							step: 0.1,
+							min: 0,
+							max: 1,
+							type: 'number'
+						}}
+						onInput={(e)=>{updateColor(colorHex, e.target.value);}}
+					/>
+				</Grid>
 			</Grid>
-			<Grid item>
-				<Input 
-					disableUnderline={true}
-					type='color'
-					style={{width:'50px', height:'20px'}}
-					onInput={(e)=>{updateColor(e.target.value, colorAlpha);}}
-					value={colorHex}
-				/>
-			</Grid>
-			<Grid item>
-				<Input
-					size='small'
-					style={{marginLeft:4}}
-					value={colorAlpha}
-					inputProps={{
-						step: 0.1,
-						min: 0,
-						max: 1,
-						type: 'number'
-					}}
-					onInput={(e)=>{updateColor(colorHex, e.target.value);}}
-				/>
-			</Grid>
-		</Grid>
-	</Box>
+		</Box>
 	)
 }
