b8ce2e8a9171273bace6d59bc52742372ecf7c73
Taylor Hanayik <hanayik@gmail.com>
2022-07-12 23:05:16 +0100

    update pages build

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.',
+  base: './',
 	server: {
   	fs: {
       // Allow serving files from one level up to the project root
